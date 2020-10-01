7676009924054919d4bd18520a606fb98c4db852
Juan Cruz Perez <juanperezdevop@gmail.com>
2020-09-30 21:24:29 -0300

    docs agregado

---
+++ docs/precache-manifest.b9b689e5464be037d3cd2840e1c9b144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91d38e4831cfe456e9216d60e008e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3b3af769af42e165f97",
+    "url": "/static/css/main.9bdf4420.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b3af769af42e165f97",
+    "url": "/static/js/main.642b5bf4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
